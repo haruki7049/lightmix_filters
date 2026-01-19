b179b53845b21259a866dd880d1f029259b2e264
haruki7049 <tontonkirikiri@gmail.com>
2026-01-19 17:48:17 +0900

    Update

--- src/volume.zig
+++ src/volume.zig
@@ -54,23 +54,3 @@ test "decay" {
 
     try std.testing.expectEqualSlices(f64, test_data.decay, decayed_wave.samples);
 }
-
-pub fn amp(original_wave: Wave) !Wave {
-    var result = std.ArrayList(f32).init(original_wave.allocator);
-
-    for (original_wave.data, 0..) |data, i| {
-        const volume: f32 = @as(f32, @floatFromInt(i)) * (1.0 / @as(f32, @floatFromInt(original_wave.data.len))) + 1.0;
-
-        const new_data = data * volume;
-        try result.append(new_data);
-    }
-
-    return Wave{
-        .data = try result.toOwnedSlice(),
-        .allocator = original_wave.allocator,
-
-        .sample_rate = original_wave.sample_rate,
-        .channels = original_wave.channels,
-        .bits = original_wave.bits,
-    };
-}

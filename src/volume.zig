dba890ac9589af56525232464906c447d84ea511
haruki7049 <tontonkirikiri@gmail.com>
2026-02-04 14:26:06 +0900

    Update volume.zig

--- src/volume.zig
+++ src/volume.zig
@@ -111,3 +111,29 @@ test "cutAttack" {
 
     try std.testing.expectEqualSlices(f64, test_data.decay, wave.samples);
 }
+
+fn normalize(comptime T: type, original_wave: Wave(T)) !Wave(T) {
+    const allocator = original_wave.allocator;
+    var result: std.array_list.Aligned(T, null) = .empty;
+
+    var max_volume: T = 0.0;
+    for (original_wave.samples) |sample| {
+        if (@abs(sample) > max_volume)
+            max_volume = @abs(sample);
+    }
+
+    for (original_wave.samples) |sample| {
+        const volume: T = 1.0 / max_volume;
+
+        const new_sample: T = sample * volume;
+        try result.append(allocator, new_sample);
+    }
+
+    return Wave(T){
+        .samples = try result.toOwnedSlice(allocator),
+        .allocator = allocator,
+
+        .sample_rate = original_wave.sample_rate,
+        .channels = original_wave.channels,
+    };
+}
